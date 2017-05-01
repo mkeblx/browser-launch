911016e854b2b924d7ea3efc745046594015e7ec
m.blix <m.blix@samsung.com>
2017-05-01 15:53:00 -0700

    SI custom tab launch init

--- app/src/main/java/com/samsung/launchintent/MainActivity.java
+++ app/src/main/java/com/samsung/launchintent/MainActivity.java
@@ -1,10 +1,14 @@
 package com.samsung.launchintent;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +20,11 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LaunchIntent";
 
     public static final String PACKAGE_SI = "com.sec.android.app.sbrowser";
+    public static final String PACKAGE_SI_BETA = "com.sec.android.app.sbrowser.beta";
+
+    public static final String SI_CUSTOM_TABS_CONNECTION =
+            "android.support.customtabs.action.CustomTabsService";
 
     public static final String PACKAGE_SI_GEARVR = "com.sec.android.app.svrbrowser";
     public static final String ACTIVITY_SI_GEARVR = PACKAGE_SI_GEARVR+".UnityPlayerNativeActivity";
@@ -34,6 +47,11 @@ public class MainActivity extends AppCompatActivity {
     public static final String PACKAGE_OCULUS_BROWSER = "com.oculus.browser";
 
     public static final String DEFAULT_URL = "https://webvr.info/samples/";
+
+
+    public static CustomTabsServiceConnection mServiceConnection;
+
+    private ServiceConnectionCallback mConnectionCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         urlText.setText(DEFAULT_URL);
 
         Button siBtn = (Button) findViewById(R.id.siLaunch);
+        Button sicustomBtn = (Button) findViewById(R.id.sicustomLaunch);
         Button oculusBtn = (Button) findViewById(R.id.oculusLaunch);
 
         siBtn.setOnClickListener(new View.OnClickListener(){
@@ -59,6 +78,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String url = urlText.getText().toString();
                 Intent intent = getSIIntent(url);
+                launchActivity(intent);
+            }
+        });
+
+        sicustomBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String url = urlText.getText().toString();
+                Intent intent = getSICustomTabIntent(url);
                 launchActivity(intent);
             }
         });
@@ -71,6 +99,15 @@ public class MainActivity extends AppCompatActivity {
                 launchActivity(intent);
             }
         });
+
+        // setup Samsung custom tab behavior
+        mServiceConnection = new ServiceConnection(null);
+        
+        Intent serviceIntent = new Intent(SI_CUSTOM_TABS_CONNECTION);
+        serviceIntent.setPackage(PACKAGE_SI);
+        bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE |
+                Context.BIND_WAIVE_PRIORITY);
+
 
 
         if (!isPackageInstalled(PACKAGE_SI_GEARVR))
@@ -98,6 +135,26 @@ public class MainActivity extends AppCompatActivity {
         intent.setData(Uri.parse("apk://"+PACKAGE_OCULUS_BROWSER));
 
         intent.putExtra("uri", uri);
+
+        return intent;
+    }
+
+
+
+    public Intent getSICustomTabIntent(String uri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+
+        // Extra used to match the session.
+        final String EXTRA_SESSION = "android.support.customtabs.extra.SESSION";
+        Bundle extras = new Bundle();
+        extras.putBinder(EXTRA_SESSION, null);
+        intent.putExtras(extras);
+        intent.setData(Uri.parse(uri));
+        intent.setPackage(PACKAGE_SI);
+
+        final String CUSTOM_TABS_TOOLBAR_COLOR = "android.support.customtabs.extra.TOOLBAR_COLOR";
+        int color = Color.MAGENTA; // todo: change to SI purple
+        intent.putExtra(CUSTOM_TABS_TOOLBAR_COLOR, color);
 
         return intent;
     }
---
+++ app/src/main/java/com/samsung/launchintent/ServiceConnectionCallback.java
@@ -0,0 +1,17 @@
+package com.samsung.launchintent;
+
+import android.support.customtabs.CustomTabsClient;
+
+public interface ServiceConnectionCallback {
+    /**
+     * Called when the service is connected.
+     *
+     * @param client a CustomTabsClient
+     */
+    void onServiceConnected(CustomTabsClient client);
+
+    /**
+     * Called when the service is disconnected.
+     */
+    void onServiceDisconnected();
+}
